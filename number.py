152af46c59ed0a5d06418bbe335697f3682c9a6c
twairgroup <39320306+twairgroup@users.noreply.github.com>
2022-01-19 20:55:10 -0500

    Python的数字逻辑
    在这里，我们将学会如何使用Python显示数字以及运算。
    Python对数字处理逻辑和其他语言一样分为两种情况：
    浮点运算（数字带有小数点的计算方式）
    整数运算（没有小数点的计算方式）
    我们可以直接在Python里使用+ - / *四种符号进行运算
    他们分别是：
    +（加号）例如1+1=2
    -（减号）例如2-1=1
    /（除号）例如4/2=2
    *（乘号）例如2*5=10
    另外*（乘号有一个特殊的用法）
    2**3=8
    **（次方运算）
    希望你动手试试看，就能明白其中的奥秘

---
+++ number.py
@@ -0,0 +1,12 @@
+age="23"
+messege="Happy brithday "+age+"rd Brithday!"
+print(messege)
+
+print(4+4)
+print(2*4)
+print(16/2)
+print(2**3)
+
+ass="0324"
+messege_1="My brithday number is "+ass+" !"
+print(messege_1)
