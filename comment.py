15125fc9d13340434e5575d14e7545447e8f1f0a
twairgroup <39320306+twairgroup@users.noreply.github.com>
2022-01-19 21:28:48 -0500

    Add files via upload

---
+++ comment.py
@@ -0,0 +1,8 @@
+# twairgroup于2022年1月19日21:04:29开发此程序
+#该程序为基础的Python Print用法例子
+first_name="ZIQIN"
+last_name="WANG"
+messege_1="你好 "+first_name +last_name
+messege_2="欢迎您使用本程序"
+messege_full=messege_1+" "+messege_2
+print(messege_full)
