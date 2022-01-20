43c543aaf094c36d0af49e4854be25916a7036d8
twairgroup <39320306+twairgroup@users.noreply.github.com>
2022-01-19 20:49:32 -0500

    关于如何使用Python显示文字以及定义函数的用法

---
+++ Projectpractice1.py
@@ -0,0 +1,7 @@
+frist_name="ZIQIN"
+last_name="WANG"
+full_name=frist_name+" "+last_name
+messege_1="Hello, "+full_name.lower()+"!"
+print(messege_1)
+messege_2="language:\nChinese\nEnglish\nJapanese"
+print(messege_2)
